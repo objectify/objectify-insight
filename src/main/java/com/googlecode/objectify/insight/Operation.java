1a4186f273c0b801c361dbe0bce9175288e21d01
Jeff Schnitzer <jeff@infohazard.org>
2014-10-01 23:02:53 -0700

    Temporarily restore Operation.LOAD
    Allows deserialization of anything still in the pull queue

--- src/main/java/com/googlecode/objectify/insight/Operation.java
+++ src/main/java/com/googlecode/objectify/insight/Operation.java
@@ -6,6 +6,9 @@ import com.google.api.client.util.Value;
  * Possible operations we track as a dimension
  */
 public enum Operation {
+	@Deprecated
+	@Value LOAD,	// temporarily here for backwards compatibility; will be removed in next version
+
 	@Value GET,
 	@Value QUERY,
 	@Value INSERT,
